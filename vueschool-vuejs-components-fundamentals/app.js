6c34fe4d03323ca9c8f6b5466181e1ea792a2ec9
Tiberiu Ion <tiberiu.vi@gmail.com>
2021-02-10 22:50:29 +0000

    Reusable components with props end
    - remove comments

--- vueschool-vuejs-components-fundamentals/app.js
+++ vueschool-vuejs-components-fundamentals/app.js
@@ -1,11 +1,8 @@
 Vue.component('plan', {
     template: '#plan-template',
-    //props also accepts an object where the props become keys and the data type is the value
-    //props also accept other objects. This opens up options to set the type, default value and even if required or not.
     props:{
         name: {
             type: String,
-            default: 'The Essential',
             required: true
         }
     }
@@ -13,7 +10,6 @@ Vue.component('plan', {
 
 new Vue({
     el:'#app',
-    //add data with  plans array to loop through in the markup
     data: {
         plans: ['The Single','The Curious', 'The Addict']
     }
