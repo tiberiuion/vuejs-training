b99f1f715fbe3b3c24938059828715e59adc23a3
Tiberiu Ion <tiberiu.vi@gmail.com>
2021-02-11 22:05:11 +0000

    register local component to global one

--- vueschool-vuejs-components-fundamentals/app.js
+++ vueschool-vuejs-components-fundamentals/app.js
@@ -1,18 +1,6 @@
-//this is a globally registered component
-//generally it's not ideal
-Vue.component('plan-picker', {
-    template: '#plan-picker-template',
-    //data now becomes a function?
-    data() {
-        return {
-           plans: ['The Single','The Curious', 'The Addict']
-        }
-    }
-})
-
-
 //locally defined component
 //can be importent into other components to be used and discarded as needed
+//cannot be used outside the components where it's defined
 let PlanComponent = {
     template: '#plan-template',
     props:{
@@ -22,6 +10,25 @@ let PlanComponent = {
         }
     }
 }
+
+//this is a globally registered component
+//generally it's not ideal
+Vue.component('plan-picker', {
+    template: '#plan-picker-template',
+    //components is an object
+    components: {
+        //it goes
+        //tagname (that will eventually be used in HTML tempalte): name of the variable in which the component was defined
+        //plan component cannot be used outside plan-picker component
+        plan: PlanComponent
+    },
+    data() {
+        return {
+           plans: ['The Single','The Curious', 'The Addict']
+        }
+    }
+})
+
 
 new Vue({
     el:'#app'
