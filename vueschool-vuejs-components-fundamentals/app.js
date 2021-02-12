7e0de0743655f77c472dde0f2c02d54059431bbd
Tiberiu Ion <tiberiu.vi@gmail.com>
2021-02-12 22:21:27 +0000

    communication between components with custom events start

--- vueschool-vuejs-components-fundamentals/app.js
+++ vueschool-vuejs-components-fundamentals/app.js
@@ -1,6 +1,3 @@
-//locally defined component
-//can be importent into other components to be used and discarded as needed
-//cannot be used outside the components where it's defined
 let PlanComponent = {
     template: '#plan-template',
     props:{
@@ -11,14 +8,9 @@ let PlanComponent = {
     }
 }
 
-//this is now a locally registered component
 let PlanPickerComponent = {
     template: '#plan-picker-template',
-    //components is an object
     components: {
-        //it goes
-        //tagname (that will eventually be used in HTML tempalte): name of the variable in which the component was defined
-        //plan component cannot be used outside plan-picker component
         plan: PlanComponent
     },
     data() {
@@ -28,13 +20,9 @@ let PlanPickerComponent = {
     }
 }
 
-
 new Vue({
     el:'#app',
-    //usually good practice to register one component globally and execute code inside it
     components: {
-        //same principle as above, we want tag-name: component name
-        //Vue is smart and can convert camelcase to kebab-case like so we can use PlanPicker and still use <plan-picker> in the HTML
         'plan-picker': PlanPickerComponent
     }
 })
