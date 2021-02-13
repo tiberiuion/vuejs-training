d9a4732007325e1f2c0533f9e4cdf6afd60c28da
Tiberiu Ion <tiberiu.vi@gmail.com>
2021-02-13 20:12:38 +0000

    pass data to parent component to make it aware of what plan is selected

--- vueschool-vuejs-components-fundamentals/app.js
+++ vueschool-vuejs-components-fundamentals/app.js
@@ -15,6 +15,9 @@ let PlanComponent = {
     //add method that turns selected property to true
     methods: {
         select() {
+            //emits event called select 
+            //first prameter is mandatory but the second one is optional. In this case the second param is some data and this is called a payload
+            this.$emit('select', this.name)
             this.selected = true
         }
     }
@@ -27,7 +30,13 @@ let PlanPickerComponent = {
     },
     data() {
         return {
-           plans: ['The Single','The Curious', 'The Addict']
+           plans: ['The Single','The Curious', 'The Addict'],
+           selectedPlan: null
+        }
+    },
+    methods: {
+        selectPlan(plan) {
+            this.selectedPlan = plan
         }
     }
 }
