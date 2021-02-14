8acebae0c727369962802b9b57c7f3121ecfee33
Tiberiu Ion <tiberiu.vi@gmail.com>
2021-02-14 12:16:59 +0000

    component naming best practices start

--- vueschool-vuejs-components-fundamentals/app.js
+++ vueschool-vuejs-components-fundamentals/app.js
@@ -14,11 +14,8 @@ let PlanComponent = {
             return this.name === this.selectedPlan
         }
     },
-    //add method that turns selected property to true
     methods: {
         select() {
-            //emits event called select 
-            //first prameter is mandatory but the second one is optional. In this case the second param is some data and this is called a payload
             this.$emit('select', this.name)
         }
     }
